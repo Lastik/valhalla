66e4f3c852f874bf3e5d3c409d7c9d0de7250d58
Dmitry Gryzunov <dg@treatface.ru>
2016-07-02 13:10:19 +0300

    Slightly changed defence calculation formula.

--- src/main/scala/core/model/stats/Defence.scala
+++ src/main/scala/core/model/stats/Defence.scala
@@ -5,7 +5,7 @@ import core.model.Damage
 case class Defence(value: Int) {
 
   def calculateWentThroughDamage(damage: Damage): Damage = {
-    damage * (-1.0 / (value / 5.0 + 1.0) + 1.0)
+    damage * (-1.0 / (value / 50.0 + 1.0) + 1.0)
   }
 }
 
